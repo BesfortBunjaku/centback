2731c8ea11e7197395d7be88ff4eb15b20c3b791
BesfortBunjaku <bessffort@gmail.com>
2023-11-12 18:08:44 +0100

    Backup

---
+++ config/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+def get_application():
+    _app = FastAPI()
+
+    _app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    return _app
+
+
+app = get_application()
---
+++ config/settings.py
@@ -0,0 +1,6 @@
+import pkg_resources
+
+
+DATA_DIR = pkg_resources.resource_filename('centback', 'data\\')
+URLS_DIR = pkg_resources.resource_filename('centback', 'urls\\')
+
